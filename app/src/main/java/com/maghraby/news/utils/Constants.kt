df8dc7445b5b860a657a144dfc539b6f86a26f45
Moustafa Maghraby <81174657+Maghraby1@users.noreply.github.com>
2022-09-12 18:11:07 +0200

    Update Constants.kt

--- app/src/main/java/com/maghraby/news/utils/Constants.kt
+++ app/src/main/java/com/maghraby/news/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.maghraby.news.utils
 
 object Constants {
 
-    val ACCESS_KEY : String = "149e2c404ef9fadc9780a0f23e103b30"
+    val ACCESS_KEY : String = "1xxxxxxxxxx"
     val BASE_URL : String = "http://api.mediastack.com/v1/"
 
 }
